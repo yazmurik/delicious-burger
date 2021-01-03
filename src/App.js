c4a52126cd12e83520b564cdf8e500d39d63e696
yazmurat <yazmurat22@gmail.com>
2021-01-03 17:39:23 -0500

    Created login page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,62 @@
 import React from 'react';
 import Login from './components/Login'
-import logo from './logo.svg';
+import logo from './burger-logo.png';
 import './App.css';
 
-class App extends React.Component() {
-  render(){
-    return (
-      <div className="App">
-        <img src={logo} className="Delicious Burger" alt="logo" />
-        <hr></hr> 
-        <Login/>
-
-
-      </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username:'',
+      password:'',
+      loggedIn:false
+    };
+    this.usercheck = this.usercheck.bind(this);
+    this.setUserName = this.setUserName.bind(this);
+    this.setPassword = this.setPassword.bind(this);
   }
   
+  usercheck(){
+    if (this.state.username === "abc" && this.state.password === "123" ){
+      this.setState({
+        loggedIn:true
+      })
+    } else {
+      alert('Username and Password doesn\'t match');
+    }
+  }
+
+  setUserName(UserName){
+    this.setState({
+     username:UserName
+    })
+  }
+
+  setPassword(UserPassword){
+    this.setState({
+     password:UserPassword
+    })
+  }
+
+  render(){
+
+    let currComponent;
+    if(this.state.loggedIn) {
+      currComponent = <h1>You are successfully Logged in</h1>
+    } else {
+      currComponent = <Login userName={this.setUserName}
+      userpassword={this.setPassword} checkUser={this.usercheck}/>
+    }
+
+    return (
+      <div className="App">
+        <img src={logo} alt="Delicious Burger" id="logo" />
+        <hr></hr>
+        {currComponent}
+      </div>
+    );
+  }
+
 }
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,20 @@
-function Login (){
+function Login(props) {
+  function handleUsername(e){
+    props.userName(e.target.value)
+  }
+  function handlePassword(e){
+    props.userpassword(e.target.value)
+  }
   return (
     <form>
       <h1>Login to start creating a burger!</h1>
-      <label htmlFor="username">Username</label>
-      <input type="text" name="username" id="username"/>
+      <label htmlFor="username">Username: </label>
+      <input type="text" name="username" id="username" autoComplete="off" onInput={handleUsername} />
       <br/>
-      <label htmlFor="user-pw">Password:</label>
-      <input id="user-pw" type="password" name="user-pw"/>
+      <label htmlFor="user-pw">Password: </label>
+      <input id="user-pw" type="password" name="user-pw" onInput={handlePassword} />
       <br/>
-      <input type="button" value="Login"/>
+      <input type="button" value="Login" onClick={props.checkUser}/>
     </form>
   )
 }
